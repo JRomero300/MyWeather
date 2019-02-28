4001039b6ebed35e7de5655f10a195efe5bee1c8
Jose Romero <jose.arcangel.romero@gmail.com>
2019-02-28 01:24:54 -0500

    updated

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Titles from "./componets/Titles";
+import Form from "./componets/Form";
+import Weather from "./componets/Weather";
+
+const API_KEY = "6c3072d9f3cf1d298a5a4833286f0253";
 
 class App extends Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=kitchener&appid=${API_KEY}"
+    );
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
