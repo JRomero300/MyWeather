9846bf23f5ff13779b3b41f7c84952d41474db4c
Jose Romero <jose.arcangel.romero@gmail.com>
2019-03-01 23:36:35 -0500

    the first commit

---
+++ weather-app/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Titles from "./componets/Titles";
+import Form from "./componets/Form";
+import Weather from "./componets/Weather";
+
+const API_KEY = "6c3072d9f3cf1d298a5a4833286f0253";
+
+class App extends Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=kitchener&appid=${API_KEY}"
+    );
+    const data = await api_call.json();
+  };
+
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
+      </div>
+    );
+  }
+}
+
+export default App;
