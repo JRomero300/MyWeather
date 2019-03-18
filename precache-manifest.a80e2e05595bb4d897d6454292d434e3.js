fde237efe31c437450ea93dfb2bb26a70614bc36
Jose Romero <jose.arcangel.romero@gmail.com>
2019-03-18 15:18:51 -0400

    Updates

---
+++ precache-manifest.a80e2e05595bb4d897d6454292d434e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/MyWeather/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/MyWeather/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "7fa70e24da2c20f131fc",
+    "url": "/MyWeather/static/js/runtime~main.7fa70e24.js"
+  },
+  {
+    "revision": "3d871f893e8e7131100f",
+    "url": "/MyWeather/static/js/main.3d871f89.chunk.js"
+  },
+  {
+    "revision": "e2a4fca7c1285f971d4a",
+    "url": "/MyWeather/static/js/2.e2a4fca7.chunk.js"
+  },
+  {
+    "revision": "3d871f893e8e7131100f",
+    "url": "/MyWeather/static/css/main.555d3acf.chunk.css"
+  },
+  {
+    "revision": "76199fc8397d279e171c71623a86acdf",
+    "url": "/MyWeather/index.html"
+  }
+];
